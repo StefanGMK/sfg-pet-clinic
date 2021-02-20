7127bcdb30ec07557e2d5f2ff3774a035e895285
ASSECOSEEBA\stefan.glamocak <stefan.glamocak@asseco-see.ba>
2021-02-20 14:56:51 +0100

    adding maven release plugin. closes #3

---
+++ pet-clinic-web/src/test/java/guru/springframework/SfgPetClinicApplicationTest.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class SfgPetClinicApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
